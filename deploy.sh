901120744a18b098a0619c1bc1fb221fd1b7919c
99jungwon <gng06128@gmail.com>
2025-04-14 09:19:54 +0900

    Auto deploy from: Linux seo-VirtualBox 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 OS=$(uname -a)
-git pull origin main
+
+# 병합 중이면 중단
+if [ -f .git/MERGE_HEAD ]; then
+  echo "🚫 병합이 완료되지 않아 중단됨. git merge --abort 또는 수동 해결 필요"
+  exit 1
+fi
+
+git pull origin main --no-rebase
 git add .
 git commit -m "Auto deploy from: $OS"
 git push origin main
+
